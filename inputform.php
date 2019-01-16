b39bc4c02d5aa22d8a4351dc899284bd0792a0af
g-rua <rua4183@gmail.com>
2019-01-16 14:21:45 +0900

    Create inputform.php

---
+++ inputform.php
@@ -0,0 +1,20 @@
+<html>
+<h1>仮フォーラム</h1>
+
+<p>背景？ </p>
+<img src="~/share/back.png">
+
+<h2>入力</h2>
+<body>
+<form method="get" action="getid.php">
+
+<p>id: <INPUT type="text" name="input1" maxlength="5"></p>
+<p>pass:<INPUT type="text" name="input2" maxlength="4"></p>
+
+<p><INPUT type="submit"></p>
+</form>
+</body>
+<p><?php echo "login"; ?></p>
+</html>
+
+
